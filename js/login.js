7d7724ecede42ad432754a7f789683e1eadfa5c3
Carlos <skhcri@gmail.com>
2021-12-01 17:30:25 +0100

    minor bugfixes

--- js/login.js
+++ js/login.js
@@ -1,59 +1,46 @@
-
-window.onload = function(){
-
+window.onload = function(){  
     var logBtn = document.getElementById('botonLog');
+    var desacBtn = document.getElementById("botonDesactiva");
+    var registroBtn = document.getElementById("crear");
+    var user = document.getElementById("username");
+
     logBtn.addEventListener('click',(e)=>{
         e.preventDefault();
         activa();
     })
-
-    var desacBtn = document.getElementById("botonDesactiva");
+    
     desacBtn.addEventListener("click", (e)=>{
         e.preventDefault();
         desactiva();
     })
-
-    var registroBtn = document.getElementById("crear");
+    
     registroBtn.addEventListener("click", (e)=>{
         e.preventDefault();
         registro();
     })
-
-    var user = document.getElementById("username");
+    
     user.addEventListener("click", ()=>{
         e.preventDefault();
         usuario();
     })
-
 }
 
 function activa(){
-
     document.getElementById("login").style.display = "block";
-
 }
 
 function desactiva(){
-
     document.getElementById("login").style.display = "none";
-
 }
 
-function registro(){
-    
+function registro(){    
     document.getElementById("registro").style.display ="block";
-
     document.getElementById("login").style.display = "none";
-
 }
 
 function usuario(event){
-
     if(document.getElementById("username") == ""){
-
         alert("El campo usuario no puede estar vacio");
-
         event.preventDefault();
     }
-
 }
--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,11 @@
-window.onload = function(){
-    
+window.addEventListener('load',()=>{
     const ham = document.querySelector('.ham');
     const enlaces = document.querySelector('.enlaces-menu');
     const barras = document.querySelectorAll('.ham span');
 
-
-    ham.addEventListener('click', () => {
-    enlaces.classList.toggle('activado');
-    barras.forEach(child => {child.classList.toggle('animado')});
-    ham.classList.toggle('girar');
+    ham.addEventListener('click', () => {        
+        enlaces.classList.toggle('activado');
+        barras.forEach(child => {child.classList.toggle('animado')});
+        ham.classList.toggle('girar');
+    })
 });
-}
